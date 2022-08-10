7c8e4aa752f2e10fd04441183f6abe0113420fe4
John Njuki <njukijohn2000@gmail.com>
2022-08-11 00:26:33 +0300

    Add Navigation Component

--- app/src/main/java/com/example/imagy/MainActivity.kt
+++ app/src/main/java/com/example/imagy/MainActivity.kt
@@ -2,29 +2,29 @@ package com.example.imagy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.add
-import androidx.fragment.app.commit
+import androidx.navigation.fragment.NavHostFragment
 import com.example.imagy.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity(R.layout.activity_main) {
+class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         supportActionBar?.hide()
 
-        supportFragmentManager.commit {
-            setReorderingAllowed(true)
-            add<MainFragment>(R.id.fragment_main_container_view)
-        }
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
 
         binding.bottomNavigation.setOnItemSelectedListener { menuItem ->
             when (menuItem.itemId) {
-                R.id.home_page -> {
+                R.id.mainFragment -> {
+                    navController.navigate(R.id.action_searchFragment_to_mainFragment)
                     true
                 }
-                R.id.search_page -> {
+                R.id.searchFragment -> {
+                    navController.navigate(R.id.action_mainFragment_to_searchFragment)
                     true
                 }
                 else -> false
